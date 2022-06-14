82b1a0e0a8857e1ba066226e4c1b10b8d8a5a6b7
RichardHorky <richardho@volny.cz>
2022-06-14 08:36:35 +0200

    initial commit

---
+++ AsyncPlay/ICalc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncPlay
+{
+    public interface ICalc
+    {
+        void CalcResult(double a, double b, double c, Action<double?, double?> result);
+    }
+}
---
+++ AsyncPlay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
